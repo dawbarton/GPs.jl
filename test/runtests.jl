1b9c2af7843ebd4017bdfe493c5465549508f135
David AW Barton <david.barton@bristol.ac.uk>
2017-10-10 21:07:43 +0100

    Added additional metric tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,13 @@ using StaticArrays
 
         x1 = @SVector [1.3, 2.2, 4.2]
         x2 = @SVector [-0.2, 2.3, 2.1]
-        iso = GPs.Metrics.Euclidean(0.5)
+        iso = GPs.Metrics.Euclidean(0.1)
+
+        @test GPs.hyperparametercount(iso) == 1
+
+        GPs.sethyperparameters!(iso, 0.5)
+
+        @test GPs.gethyperparameters(iso) == MVector(0.5)
 
         @test GPs.Metrics.sqrdistance(iso, x1, x2) ≈ 26.68
         @test GPs.Metrics.sqrdistance_dθ(iso, x1, x2) ≈ SVector(-106.72)
@@ -23,7 +29,13 @@ using StaticArrays
 
         x1 = @SVector [1.3, 2.2, 4.2]
         x2 = @SVector [-0.2, 2.3, 2.1]
-        aniso = GPs.Metrics.Euclidean(MVector(0.5, 0.2, 1.3))
+        aniso = GPs.Metrics.Euclidean(MVector(0.5, 0.2, 1.2))
+
+        @test GPs.hyperparametercount(aniso) == 3
+
+        GPs.sethyperparameters!(aniso, [0.5, 0.2, 1.3])
+
+        @test GPs.gethyperparameters(aniso) == MVector(0.5, 0.2, 1.3)
 
         @test GPs.Metrics.sqrdistance(aniso, x1, x2) ≈ 11.85946746
         @test GPs.Metrics.sqrdistance_dθ(aniso, x1, x2) ≈ SVector(-36.0, -2.5, -4.014565316)
