9185fa045df038e65593625aaa1e4ec53cd51c31
David AW Barton <david.barton@bristol.ac.uk>
2017-11-13 17:36:09 +0000

    Corrected toSVector

--- src/general.jl
+++ src/general.jl
@@ -96,7 +96,7 @@ toSVector(x::Vector{T}) where {T <: Number} = reinterpret(SVector{1, T}, x, (len
 toSVector(x::Vector{SVector{N, T}}) where {N, T <: Number} = x
 toSVector(x::Matrix{T}) where {T <: Number} = reinterpret(SVector{size(x, 1), T}, x, (size(x, 2),))
 toSVector(x::AbstractVector{T}) where {T <: Number} = [SVector{1}(el) for el in x] # catch-all for generators, etc
-toSVector(x::AbstractVector{AbstractVector{T}}) where {T <: Number} = toSVector(hcat(x...)) # going to be horrible!
+toSVector(x::AbstractVector) = toSVector(hcat(x...)) # going to be horrible!
 
 export toSVector
 
