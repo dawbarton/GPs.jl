264bef4cf0e5bec2ce4a8119b021dd2840cb5048
David AW Barton <david.barton@bristol.ac.uk>
2017-11-13 15:18:52 +0000

    Refactoring to make training data mutable

---
+++ src/DataSets.jl
@@ -0,0 +1,44 @@
+module DataSets
+
+using ..GPs
+using StaticArrays
+
+"""
+TrainingSet{NX, NY, T}
+
+A data structure to store inputs (x) and outputs (y) for use in training a
+Gaussian process. Wherever possible data is not copied but data structures are
+reinterpreted for speed. However, this could lead to inadvertant data
+modification by the user if sufficient care is not taken.
+"""
+struct TrainingSet{NX, NY, T}
+    x::Vector{SVector{NX, T}}
+    y::Vector{SVector{NY, T}}
+    xm::Matrix{T}
+    ym::Matrix{T}
+
+    function TrainingSet(x::Vector{SVector{NX, T}}, y::Vector{SVector{NY, T}}) where {NX, NY, T <: Number}
+        if length(x) != length(y)
+            throw(ArgumentError("x and y must have the same length"))
+        end
+        new{NX, NY, T}(x,
+                       y,
+                       reinterpret(T, x, (NX, length(x))),
+                       reinterpret(T, y, (NY, length(y))))
+    end
+    function TrainingSet(x::Matrix{T}, y::Matrix{T}) where {T <: Number}
+        if size(x, 2) != size(y, 2)
+            throw(ArgumentError("x and y must have the same number of columns"))
+        end
+        new{size(x, 1), size(y, 1), T}(reinterpret(MVector{size(x, 1), T}, x, (size(x, 2),))
+                                       reinterpret(MVector{size(y, 1), T}, y, (size(y, 2),))
+                                       x,
+                                       y)
+    end
+end
+
+TrainingSet(x, y) = TrainingSet(toSVector(x), toSVector(y))
+
+samplecount(data::TrainingSet) = length(data.x)
+
+end
--- src/GaussianLikelihoods.jl
+++ src/GaussianLikelihoods.jl
@@ -2,75 +2,61 @@ module GaussianLikelihoods
 
 using ..GPs
 using ..Kernels
+using ..DataSets
 using StaticArrays
 using DocStringExtensions
-using Optim
 
 import ..GPs: sethyperparameters!, gethyperparameters, hyperparametercount,
-    optimizehyperparameters!, loglikelihood, loglikelihood_dθ, loglikelihood_dθ!
+    loglikelihood, loglikelihood_dθ, loglikelihood_dθ!
 
-mutable struct GaussianLikelihood{T, NX, NY, K <: Kernel{T}} <: AbstractGP{T}
+struct GP{T, NX, NY, K <: Kernel{T}} <: AbstractGP{T}
     kernel::K # covariance function
-    σn::T # additive noise (MVector so it remains mutable)
-    x::Vector{SVector{NX, T}} # input data
-    y::Matrix{T} # output data (rows are individual samples)
+    σn::MVector{1, T} # additive noise (MVector so it remains mutable)
+    training::TrainingSet{NX, NY, T}
     K::UpperTriangular{T, Matrix{T}} # Covariance matrix
     L::UpperTriangular{T, Matrix{T}} # Cholesky factors of the covariance matrix
     α::Matrix{T} # α = K⁻¹y
 
-    function GaussianLikelihood(kernel::Kernel, σn::T, x::Vector{SVector{NX, T}}, y::Matrix{T}) where {NX, T <: Number}
-        n = length(x) # number of samples
-        m = size(y, 1) # number of output dimensions
-        if n != size(y, 2)
-            throw(DimensionMismatch("The number of samples in x and y must match (i.e. length(x) == size(y, 1))"))
-        end
-        gp = new{T, NX, m, typeof(kernel)}(kernel,
-                                           σn,
-                                           Vector{SVector{NX, T}}(n), # x
-                                           Matrix{T}(n, m), # y
-                                           UpperTriangular(Matrix{T}(n, n)), # K
-                                           UpperTriangular(Matrix{T}(n, n)), # L = chol(K)
-                                           Matrix{T}(n, m)) # α = K⁻¹y
-        # Copy data to avoid unexpected problems in bad user code later
-        gp.x .= x
-        gp.y .= y' # assume we given samples as columns
+    function GP(kernel::Kernel, σn::T, training::TrainingSet{NX, NY}) where {T <: Number, NX, NY}
+        n = samplecount(training) # number of samples
+        gp = new{T, NX, NY, typeof(kernel)}(kernel,
+                                            MVector(σn),
+                                            training,
+                                            UpperTriangular(Matrix{T}(n, n)), # K
+                                            UpperTriangular(Matrix{T}(n, n)), # L = chol(K)
+                                            Matrix{T}(n, NY)) # α = K⁻¹y
         # Update with the specified hyperparameters/observations
         update!(gp)
         return gp
     end
 end
 
-GaussianLikelihood(kernel::Kernel{T}, σn::T, x, y) where {T} = GaussianLikelihood(kernel, σn, toSVector(x)::Vector{SVector{NX, T}} where {NX}, toMatrix(y)::Matrix{T})
+export GP
 
-const GP = GaussianLikelihood
-
-export GaussianLikelihood, GP
-
-function sethyperparameters!(gp::GaussianLikelihood, θ)
+function sethyperparameters!(gp::GP, θ)
     # Recursively set the hyperparameters
-    gp.σn = θ[1]
+    gp.σn[1] = θ[1]
     sethyperparameters!(gp.kernel, θ[2:end])
     # Update the GP with the new hyperparameters
     update!(gp)
 end
 
-gethyperparameters(gp::GaussianLikelihood) = [SVector(gp.σn[1]); gethyperparameters(gp.kernel)]
+gethyperparameters(gp::GP) = [SVector(gp.σn); gethyperparameters(gp.kernel)]
 
-hyperparametercount(gp::GaussianLikelihood) = hyperparametercount(gp.kernel) + 1
+hyperparametercount(gp::GP) = hyperparametercount(gp.kernel) + 1
 
 function update!(gp::GP)
     # Prior covariance using the specified kernel
-    covariance_matrix!(gp.K, gp.kernel, gp.x)
+    covariance_matrix!(gp.K, gp.kernel, gp.training.x)
     # Add measurement noise at each point
-    n = length(gp.x)
-    for i in 1:n+1:n*n
-        gp.K[i] += gp.σn^2
+    for i ∈ diagind(gp.K)
+        gp.K[i] += gp.σn[1]^2
     end
     # Calculate the Cholesky decomposition
     copy!(gp.L, gp.K)
     cholfact!(Symmetric(gp.L)) # assumes L is upper triangular
     # Calculate quantities required for interpolation
-    gp.α .= gp.y
+    gp.α .= gp.ym'
     A_ldiv_B!(gp.L', gp.α) # L' \ y (intermediate)
     A_ldiv_B!(gp.L, gp.α) # α = L \ (L' \ y)
     return gp
@@ -85,8 +71,8 @@ $(SIGNATURES)
 
 Return the mean of the Gaussian Process at the specified points.
 """
-function mean(gp::GaussianLikelihood{T}, x::Vector{SVector{N, T}}) where {T, N}
-    Kₛ = covariance_matrix(gp.kernel, x, gp.x)
+function mean(gp::GP{T}, x::Vector{SVector{N, T}}) where {T, N}
+    Kₛ = covariance_matrix(gp.kernel, x, gp.training.x)
     return At_mul_Bt(gp.α, Kₛ) # (Kₛ*α)ᵀ
 end
 
@@ -97,11 +83,11 @@ $(SIGNATURES)
 
 Return the variance of the Gaussian Process at the specified points.
 """
-function var(gp::GaussianLikelihood{T}, x::Vector{SVector{N, T}}) where {T, N}
-    Kₛ = covariance_matrix(gp.kernel, gp.x, x)
+function var(gp::GP{T}, x::Vector{SVector{N, T}}) where {T, N}
+    Kₛ = covariance_matrix(gp.kernel, gp.training.x, x)
     v = gp.L' \ Kₛ
     Kₛₛ = [covariance(gp.kernel, xᵢ, xᵢ) for xᵢ in x]
-    return Kₛₛ - vec(sum(v.*v, 1)) + gp.σn^2
+    return Kₛₛ - vec(sum(v.*v, 1)) + gp.σn[1]^2
 end
 
 var(gp::GaussianLikelihood{T}, x::Union{AbstractVector{T}, Matrix{T}}) where {T} = var(gp, toSVector(x))
@@ -111,12 +97,12 @@ $(SIGNATURES)
 
 Return the covariance matrix of the Gaussian Process at the specified points.
 """
-function cov(gp::GaussianLikelihood{T}, x::Vector{SVector{N, T}}) where {T, N}
-    Kₛ = covariance_matrix(gp.kernel, gp.x, x)
+function cov(gp::GP{T}, x::Vector{SVector{N, T}}) where {T, N}
+    Kₛ = covariance_matrix(gp.kernel, gp.training.x, x)
     v = gp.L' \ Kₛ
     Kₛₛ = covariance_matrix(gp.kernel, x)
     Kₚ = Kₛₛ - v'*v
-    for i in diagind(Kₚ)
+    for i ∈ diagind(Kₚ)
         Kₚ[i] += gp.σn^2
     end
     return Kₚ
@@ -126,24 +112,24 @@ cov(gp::GaussianLikelihood{T}, x::Union{AbstractVector{T}, Matrix{T}}) where {T}
 
 #--- Likelihoods
 
-function loglikelihood(gp::GaussianLikelihood{T}) where {T}
+function loglikelihood(gp::GP{T}) where {T}
     # log likelihood of the GP itself (w.r.t. the hyperparameters)
-    n = length(gp.x)
+    n = length(gp.training.x)
     # log(det(K))
     logdetK = sum(log.(diag(gp.L)))
     # The log likelihood
     lik = T(0.5)*mean(sum(gp.y.*gp.α, 1)) + logdetK + T(0.5)*n*log(T(2)*π)
 end
 
-function loglikelihood_dθ!(dθ::AbstractVector{T}, gp::GaussianLikelihood{T}) where {T}
+function loglikelihood_dθ!(dθ::AbstractVector{T}, gp::GP{T}) where {T}
     n = length(gp.x)
     I = eye(T, n)
     At_ldiv_B!(gp.L, I) # I = gp.L' \ eye(n)
     A_ldiv_B!(gp.L, I) # I = gp.L \ (gp.L' \ eye(n))
     #inner = (gp.α*gp.α' - gp.L \ (gp.L' \ eye(n)))'
     Kdθ = @view dθ[2:end]
-    for j in 1:n
-        for i in 1:j
+    for j ∈ 1:n
+        for i ∈ 1:j
             mult = (mean(gp.α[i, :] .* gp.α[j, :]) - I[i, j]) * ifelse(i == j, T(0.5), T(1)) # use ifelse to avoid computing the symmetric counterparts
             Kdθ .-= covariance_dθ(gp.kernel, gp.x[i], gp.x[j]) .* mult
             dθ[1] -= ifelse(i == j, T(2) * gp.σn * mult, zero(T))
@@ -152,7 +138,7 @@ function loglikelihood_dθ!(dθ::AbstractVector{T}, gp::GaussianLikelihood{T}) w
     return dθ
 end
 
-loglikelihood_dθ(gp::GaussianLikelihood{T}) where {T} = loglikelihood_dθ!(zeros(T, hyperparametercount(gp)), gp)
+loglikelihood_dθ(gp::GP{T}) where {T} = loglikelihood_dθ!(zeros(T, hyperparametercount(gp)), gp)
 
 
 end
